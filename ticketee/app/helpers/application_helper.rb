fa2d5a83324b57faff3f3b578b129b7081c95e6b
ed wassermann <visavis.kid@gmail.com>
2012-08-22 16:21:34 +0200

    lock down specific projects controller actions for admins only

--- ticketee/app/helpers/application_helper.rb
+++ ticketee/app/helpers/application_helper.rb
@@ -1,4 +1,10 @@
 module ApplicationHelper
+
+  def admins_only(&block)
+  	block.call if current_user.try(:admin?)
+  	nil
+  end
+
 	def title(*parts)
 		unless parts.empty?
 			content_for :title do
